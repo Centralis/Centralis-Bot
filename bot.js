44cbdf6fd5f370612380f3b621f14b58aff6363c
Centralis <38593500+Centralis@users.noreply.github.com>
2018-07-04 14:28:55 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,22 +1,29 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
+
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
+const config = require("./config.json");
+
+client.on("ready", () => {
+  console.log(`CenBot is now online.`); 
+  client.user.setActivity(`Use !schedule for upcoming dates`);
 });
 
-client.on('message', message => {
-    if (message.content === '!help') {
-    	message.channel.send('Discord Bot Commands');
-        message.channel.send('!fornitestats <player>: Displays stats of a specific player.');
-  	}
+
+client.on("message", async message => {
+  if(message.author.bot) return;
+
+  if(message.content.indexOf(config.prefix) !== 0) return;
+  
+  const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
+  const command = args.shift().toLowerCase();
+  
+  if(command === "schedule") {
+    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+    const m = await message.channel.send("schedule?");
+    m.edit(`The next tournament is scheduled for July 7th from 12 PM EST - 4 PM EST.`);
+  }
 });
 
-client.on('message', message => {
-    if (message.content === '!fortnitestats') {
-        let player = args[0]
-    	message.reply('${player}'s Stats: https://fortnitetracker.com/profile/pc/${player}');
-  	}
-});
-
-client.login(process.env.BOT_TOKEN);
+client.login(config.token);
